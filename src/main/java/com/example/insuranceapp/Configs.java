a00de2f5093da82d36c3879211eccc568a62729d
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-15 22:26:26 +0300

    Конфіги бази даних InsuranceApp (v. 0.1.2)

---
+++ src/main/java/com/example/insuranceapp/Configs.java
@@ -0,0 +1,9 @@
+package com.example.insuranceapp;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "root1528";
+    protected String dbName = "insuranceapp";
+}
