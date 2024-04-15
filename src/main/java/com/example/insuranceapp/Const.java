2f71143ebe3af43cdd93647c67168f4c995fd1c1
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-15 22:28:01 +0300

    Константи для роботи з базою даних InsuranceApp (v. 0.1.3)

---
+++ src/main/java/com/example/insuranceapp/Const.java
@@ -0,0 +1,12 @@
+package com.example.insuranceapp;
+
+public class Const {
+    public static final String USER_TABLE = "users";
+    public static final String USER_ID = "idusers";
+    public static final String USER_FIRSTNAME = "firstname";
+    public static final String USER_SECONDNAME = "secondname";
+    public static final String USER_THIRDNAME = "thirdname";
+    public static final String USER_USERNAME = "username";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_PERMISSION = "permission";
+}
