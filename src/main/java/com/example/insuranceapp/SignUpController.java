541129894d1abbe82c6f9add854c6c40060f9b67
777DenisEvseev777 <evseevd267@gmail.com>
2024-04-17 11:47:47 +0300

    Додавання функціоналу кнопки повернутися InsuranceApp (v. 0.3.6)

--- src/main/java/com/example/insuranceapp/SignUpController.java
+++ src/main/java/com/example/insuranceapp/SignUpController.java
@@ -1,18 +1,28 @@
 package com.example.insuranceapp;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.StageStyle;
 
 public class SignUpController {
     Stage stage = new Stage();
+
+    @FXML
+    private AnchorPane registerpane;
 
     @FXML
     private ResourceBundle resources;
@@ -80,8 +90,18 @@ public class SignUpController {
         });
 
         back.setOnAction(event -> {
-            Stage stage = (Stage) back.getScene().getWindow();
-            stage.close();
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("login.fxml")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Scene scene = new Scene(root);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+            Stage currentStage = (Stage) registerpane.getScene().getWindow();
+            currentStage.close();
         });
     }
 
