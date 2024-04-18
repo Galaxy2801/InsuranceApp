b55a1e8fda597fed9d1128fd9ce938b729f16bf2
777DenisEvseev777 <evseevd267@gmail.com>
2024-04-18 21:11:08 +0300

    Додавання до програми нових вікон та контроллерів InsuranceApp (v. 0.3.10)

---
+++ src/main/java/com/example/insuranceapp/UserpanelController.java
@@ -0,0 +1,20 @@
+package com.example.insuranceapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class UserpanelController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
