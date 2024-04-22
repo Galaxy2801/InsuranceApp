80924b8cf70d5515de4683d9e02c5a448abd6251
777DenisEvseev777 <evseevd267@gmail.com>
2024-04-22 14:53:20 +0300

    Додавання властивості переміщення вікна авторизації InsuranceApp (v. 0.4.6)

--- src/main/java/com/example/insuranceapp/Main.java
+++ src/main/java/com/example/insuranceapp/Main.java
@@ -3,20 +3,41 @@ package com.example.insuranceapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+    private double windowWidth;
+    private double windowHeight;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        scene.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Поліси");
         stage.setScene(scene);
         stage.show();
+
+        windowWidth = stage.getWidth();
+        windowHeight = stage.getHeight();
     }
 
     public static void main(String[] args) {
