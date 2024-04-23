78834884f1451486611e2f17b2c803afe53adc66
777DenisEvseev777 <evseevd267@gmail.com>
2024-04-23 21:48:02 +0300

    Налаштування функціоналу кнопок Повернутися та Вихід для панелі користувача та робітника InsuranceApp (v. 0.4.10)

--- src/main/java/com/example/insuranceapp/UserpanelController.java
+++ src/main/java/com/example/insuranceapp/UserpanelController.java
@@ -1,11 +1,20 @@
 package com.example.insuranceapp;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.layout.AnchorPane;
 
 public class UserpanelController {
+    Stage stage = new Stage();
 
     @FXML
     private ResourceBundle resources;
@@ -35,8 +44,29 @@ public class UserpanelController {
     private Button exit;
 
     @FXML
-    void initialize() {
+    private AnchorPane userpane;
 
+    @FXML
+    void initialize() {
+        exit.setOnAction(event -> {
+            Stage stage = (Stage) exit.getScene().getWindow();
+            stage.close();
+        });
+
+        back.setOnAction(event -> {
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("login.fxml")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Scene scene = new Scene(root);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+            Stage currentStage = (Stage) userpane.getScene().getWindow();
+            currentStage.close();
+        });
     }
 
 }
--- src/main/java/com/example/insuranceapp/WorkerpanelController.java
+++ src/main/java/com/example/insuranceapp/WorkerpanelController.java
@@ -1,11 +1,20 @@
 package com.example.insuranceapp;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.layout.AnchorPane;
 
 public class WorkerpanelController {
+    Stage stage = new Stage();
 
     @FXML
     private ResourceBundle resources;
@@ -35,8 +44,29 @@ public class WorkerpanelController {
     private Button exit;
 
     @FXML
-    void initialize() {
+    private AnchorPane workerpane;
 
+    @FXML
+    void initialize() {
+        exit.setOnAction(event -> {
+            Stage stage = (Stage) exit.getScene().getWindow();
+            stage.close();
+        });
+
+        back.setOnAction(event -> {
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("login.fxml")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Scene scene = new Scene(root);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+            Stage currentStage = (Stage) workerpane.getScene().getWindow();
+            currentStage.close();
+        });
     }
 
 }
