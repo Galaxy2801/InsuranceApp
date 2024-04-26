05bd5df9faee6518af9d9ff8cd644bda1dce96f5
777DenisEvseev777 <evseevd267@gmail.com>
2024-04-26 22:46:55 +0300

    Налаштування функціоналу панелі робітника до розділів (транспорт, нерухомість, особа) InsuranceApp (v. 0.4.12)

--- src/main/java/com/example/insuranceapp/WorkerpanelController.java
+++ src/main/java/com/example/insuranceapp/WorkerpanelController.java
@@ -12,6 +12,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -52,6 +54,48 @@ public class WorkerpanelController {
     private AnchorPane build;
 
     @FXML
+    private TextField build_field_reg_address;
+
+    @FXML
+    private TextField build_field_reg_area;
+
+    @FXML
+    private TextField build_field_reg_roomnum;
+
+    @FXML
+    private TextField build_field_reg_user;
+
+    @FXML
+    private RadioButton build_reg_area_selector_ga;
+
+    @FXML
+    private RadioButton build_reg_area_selector_m;
+
+    @FXML
+    private Button build_reg_button;
+
+    @FXML
+    private RadioButton build_reg_cost_selector_bilshemillion;
+
+    @FXML
+    private RadioButton build_reg_cost_selector_domillion;
+
+    @FXML
+    private RadioButton build_reg_cost_selector_dosta;
+
+    @FXML
+    private RadioButton build_reg_cost_selector_dotreesta;
+
+    @FXML
+    private RadioButton build_reg_type_selector_earth;
+
+    @FXML
+    private RadioButton build_reg_type_selector_home;
+
+    @FXML
+    private RadioButton build_reg_type_selector_industrial;
+
+    @FXML
     private Button creationBuild;
 
     @FXML
@@ -70,10 +114,68 @@ public class WorkerpanelController {
     private Button exit;
 
     @FXML
+    private TextField human_field_reg_fathername;
+
+    @FXML
+    private TextField human_field_reg_name;
+
+    @FXML
+    private TextField human_field_reg_prizv;
+
+    @FXML
+    private TextField human_field_reg_year;
+
+    @FXML
+    private RadioButton human_gender_reg_man;
+
+    @FXML
+    private RadioButton human_gender_reg_woman;
+
+    @FXML
+    private RadioButton human_health_reg_invalid;
+
+    @FXML
+    private RadioButton human_health_reg_zdorov;
+
+    @FXML
+    private Button human_reg_button;
+
+    @FXML
     private AnchorPane transport;
 
     @FXML
+    private TextField transport_field_reg_color;
+
+    @FXML
+    private TextField transport_field_reg_fuel;
+
+    @FXML
+    private TextField transport_field_reg_mark;
+
+    @FXML
+    private TextField transport_field_reg_model;
+
+    @FXML
+    private TextField transport_field_reg_number;
+
+    @FXML
+    private TextField transport_field_reg_type;
+
+    @FXML
+    private TextField transport_field_reg_user;
+
+    @FXML
+    private TextField transport_field_reg_vin;
+
+    @FXML
+    private TextField transport_field_reg_volume;
+
+    @FXML
+    private Button transport_reg_button;
+
+    @FXML
     private AnchorPane workerpane;
+
 
 
     @FXML
