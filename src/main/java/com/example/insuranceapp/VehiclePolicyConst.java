fe1c79128becfff5209338c543d3a0d890bbc6e7
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-28 01:01:27 +0300

    Константи для роботи з таблицею полісів транспорту для бази даних InsuranceApp (v. 0.4.17)

---
+++ src/main/java/com/example/insuranceapp/VehiclePolicyConst.java
@@ -0,0 +1,15 @@
+package com.example.insuranceapp;
+
+public class VehiclePolicyConst {
+    public static final String POLICY_TABLE = "vehicle_policy";
+    public static final String POLICY_ID = "idvehicle_policy";
+    public static final String POLICY_CLIENT = "client";
+    public static final String POLICY_MAKER = "maker";
+    public static final String POLICY_MODEL = "model";
+    public static final String POLICY_TYPE = "type";
+    public static final String POLICY_REG_NUMBER = "reg_number";
+    public static final String POLICY_VIN_NUMBER= "vin_number";
+    public static final String POLICY_ENGINE_CAPACITY = "engine_capacity";
+    public static final String POLICY_FUEL_TYPE = "fuel_type";
+    public static final String POLICY_COLOR = "color";
+}
