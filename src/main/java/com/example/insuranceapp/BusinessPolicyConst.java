484c0e5563fb3605fdc636bbb91bfdb6a479a226
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-28 20:37:02 +0300

    Константи для роботи з таблицею полісів бізнесу (DB) InsuranceApp (v. 0.4.37)

---
+++ src/main/java/com/example/insuranceapp/BusinessPolicyConst.java
@@ -0,0 +1,13 @@
+package com.example.insuranceapp;
+
+public class BusinessPolicyConst {
+    public static final String POLICY_TABLE = "business_policy";
+    public static final String POLICY_ID = "idbusiness_policy";
+    public static final String POLICY_CLIENT = "client";
+    public static final String POLICY_NAME = "name";
+    public static final String POLICY_SERV_INDUSTRIES = "serv_industries";
+    public static final String POLICY_BUSINESS_COST = "business_cost";
+    public static final String POLICY_PROFIT_PER_YEAR = "profit_per_year";
+    public static final String POLICY_COMPENSATION = "compensation";
+    public static final String POLICY_RESPONS = "respons";
+}
