0d6b0afbe67c84a579a7dcff58c4b584432823f5
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-28 17:24:05 +0300

    Константи для роботи з таблицею полісів нерухомості (DB) InsuranceApp (v. 0.4.22)

---
+++ src/main/java/com/example/insuranceapp/BuildPolicyConst.java
@@ -0,0 +1,13 @@
+package com.example.insuranceapp;
+
+public class BuildPolicyConst {
+    public static final String POLICY_TABLE = "build_policy";
+    public static final String POLICY_ID = "idbuild_policy";
+    public static final String POLICY_CLIENT = "client";
+    public static final String POLICY_ADDRESS = "address";
+    public static final String POLICY_AREA = "area";
+    public static final String POLICY_AREA_UNIT = "area_unit";
+    public static final String POLICY_BUILD_TYPE = "build_type";
+    public static final String POLICY_NUM_OF_ROOMS = "num_of_rooms";
+    public static final String POLICY_COST = "cost";
+}
