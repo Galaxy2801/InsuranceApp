07a9543b21bfc5390b562f608a3d4c70e7a5f180
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-28 18:28:54 +0300

    Доданий клас для взаємодії з полісами для особи InsuranceApp (v. 0.4.28)

---
+++ src/main/java/com/example/insuranceapp/HumanPolicy.java
@@ -0,0 +1,77 @@
+package com.example.insuranceapp;
+
+public class HumanPolicy {
+    private String client;
+    private String firstName;
+    private String secondName;
+    private String middleName;
+    private String age;
+    private String gender;
+    private String health;
+
+    public HumanPolicy(String client, String firstName, String secondName, String age, String middleName, String gender, String health) {
+        this.client = client;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+        this.middleName = middleName;
+        this.gender = gender;
+        this.health = health;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+}
