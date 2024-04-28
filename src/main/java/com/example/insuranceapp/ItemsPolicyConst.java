489184a1d0fca25a7a5bb0de88ca1510b9043207
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-28 20:13:08 +0300

    Константи для роботи з таблицею полісів майна (DB) InsuranceApp (v. 0.4.32)

---
+++ src/main/java/com/example/insuranceapp/ItemsPolicyConst.java
@@ -0,0 +1,11 @@
+package com.example.insuranceapp;
+
+public class ItemsPolicyConst {
+    public static final String POLICY_TABLE = "items_policy";
+    public static final String POLICY_ID = "iditems_policy";
+    public static final String POLICY_CLIENT = "client";
+    public static final String POLICY_NAME = "name";
+    public static final String POLICY_COST = "cost";
+    public static final String POLICY_PROP_CONDITION = "prop_condition";
+    public static final String POLICY_COMPENSATION = "compensation";
+}
