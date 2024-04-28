6a18b09e116339202d050408674d3c08838958d9
Galaxy2801 <kuznecovvalentin1@gmail.com>
2024-04-28 18:25:44 +0300

    Константи для роботи з таблицею полісів особи (DB) InsuranceApp (v. 0.4.27)

---
+++ src/main/java/com/example/insuranceapp/HumanPolicyConst.java
@@ -0,0 +1,13 @@
+package com.example.insuranceapp;
+
+public class HumanPolicyConst {
+    public static final String POLICY_TABLE = "human_policy";
+    public static final String POLICY_ID = "idhuman_policy";
+    public static final String POLICY_CLIENT = "client";
+    public static final String POLICY_FIRSTNAME = "firstname";
+    public static final String POLICY_SECONDNAME = "secondname";
+    public static final String POLICY_MIDDLENAME = "middlename";
+    public static final String POLICY_AGE = "age";
+    public static final String POLICY_GENDER = "gender";
+    public static final String POLICY_HEALTH = "health";
+}
