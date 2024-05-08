ebab721dff9fc5ed712e3e452dc044dfdbc57504
777DenisEvseev777 <evseevd267@gmail.com>
2024-05-08 12:48:27 +0300

    Додавання функціоналу панелі СуперАдміна InsuranceApp (v. 0.5.17)

--- src/main/java/com/example/insuranceapp/AdminController.java
+++ src/main/java/com/example/insuranceapp/AdminController.java
@@ -1,14 +1,22 @@
 package com.example.insuranceapp;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class AdminController {
+    Stage stage = new Stage();
 
     @FXML
     private ResourceBundle resources;
@@ -36,6 +44,21 @@ public class AdminController {
 
     @FXML
     void initialize() {
+
+        AdminPanelBackButton.setOnAction(event -> {
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("login.fxml")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Scene scene = new Scene(root);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+            Stage currentStage = (Stage) AdminPanelBackButton.getScene().getWindow();
+            currentStage.close();
+        });
 
     }
 
